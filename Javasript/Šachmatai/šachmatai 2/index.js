3603db6cdd53c53dd4bcd5e787b484c359d510e5
Unknown <mantasmmm@gmail.com>
2018-07-24 13:21:04 +0300

    .

--- Javasript/Šachmatai/šachmatai 2/index.js
+++ Javasript/Šachmatai/šachmatai 2/index.js
@@ -1,25 +1,34 @@
 let dragged;
+let oldx;
+let oldy;
+let newx;
+let newy;
 
 document.addEventListener("dragstart", function( event ) {
     dragged = event.target;
+    oldy=event.target.parentNode.parentNode.rowIndex+1;
+    oldx=event.target.parentNode.cellIndex+1;
 }, false);
 
 document.addEventListener("dragover", function( event ) {
     event.preventDefault();
+    newy=event.target.parentNode.rowIndex+1;
+    newx=event.target.cellIndex+1;
 }, false);
 
 document.addEventListener("drop", function( event ) {
     event.preventDefault();
-    if(dragged.className === "white-Pawn"){
-        dragged.parentNode.removeChild( dragged );
-        event.target.appendChild( dragged );
+    if(dragged.className === "white-Pawn") {
+        if (newx===oldx && newy===oldy-1) {
+            dragged.parentNode.removeChild(dragged);
+            event.target.appendChild(dragged);
+        }
     }
     if(dragged.className === "black-Pawn"){
-        dragged.parentNode.removeChild( dragged );
-        event.target.appendChild( dragged );
+        if (newx===oldx && newy===oldy+1) {
+            dragged.parentNode.removeChild(dragged);
+            event.target.appendChild(dragged);
+        }
     }
 
 }, false);
-
-
-        //skirstyt kiek gali eit su cell indexu -->
